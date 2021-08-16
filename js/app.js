1648d4a00dd2d3662dd6a0e6d74cd752d71aab2b
jaehyuenjung <jaehyuen0613@gmail.com>
2021-08-16 22:45:32 +0900

    Reform to Toom Nook Talk

--- js/app.js
+++ js/app.js
@@ -1,16 +1,15 @@
 const userComponent = document.querySelectorAll(".user-component");
-const btnGifts = document.querySelectorAll(".user-component__btn-gift");
 
 let isContextMenuFocus = false;
 
 function handleUserComponentDblClick(event) {
-    console.dir(this);
+    const title = this.querySelector(".user-component__title");
+
+    if (title && title.innerText === "Raymond") {
+        location.href = "chat.html";
+    }
 }
 
-function handleBtnGiftClick(event) {
-    console.dir(this);
-}
-
-userComponent.forEach(com => com.addEventListener("dblclick", handleUserComponentDblClick));
-btnGifts.forEach(btn => btn.addEventListener("click", handleBtnGiftClick));
-
+userComponent.forEach((com) =>
+    com.addEventListener("dblclick", handleUserComponentDblClick)
+);
